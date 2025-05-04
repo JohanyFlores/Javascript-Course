04d29a19da1672ac083097d70d6fdb1f5b73b718
JohanyFlores <jfloresm1@student.obsbusiness.school>
2025-05-03 21:24:04 -0600

    Update aboutme.js
    Ejercicio 1

--- aboutme.js
+++ aboutme.js
@@ -1,3 +1,7 @@
+ export let miNombre = "Johany Flores Mateos";
+ export let miProfesion = "Psicólogo";
+ export let miPuesto = "Líder Técnico";
+
 function misDatos() {
   let miNombre = "Johany Flores Mateos";
   let miProfesion = "Psicólogo";
@@ -5,6 +9,6 @@ function misDatos() {
 
   // Variable que almacena la cadena utilizando template literals
   let presentacion = `Hola, me llamo ${miNombre} y me dedico a ${miProfesion}. Estoy cursando este Master porque me gustaría trabajar en ${miPuesto}`;
-
   console.log(presentacion);
 }
+export {miNombre, miProfesion, miPuesto};
