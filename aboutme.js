1499a0865ee64bf1ce0509cd65681d90aa56c575
JohanyFlores <jfloresm1@student.obsbusiness.school>
2025-05-03 21:05:52 -0600

    Create aboutme.js
    Actividad 1

---
+++ aboutme.js
@@ -0,0 +1,10 @@
+function misDatos() {
+  let miNombre = "Johany Flores Mateos";
+  let miProfesion = "Psicólogo";
+  let miPuesto = "Líder Técnico";
+
+  // Variable que almacena la cadena utilizando template literals
+  let presentacion = `Hola, me llamo ${miNombre} y me dedico a ${miProfesion}. Estoy cursando este Master porque me gustaría trabajar en ${miPuesto}`;
+
+  console.log(presentacion);
+}
